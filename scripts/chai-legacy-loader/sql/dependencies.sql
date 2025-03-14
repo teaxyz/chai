d991e54ba9298ef4d7c5745439c83c58ea7f0be6
Sanchit Ram Arvind <sanchitram@gmail.com>
2025-03-13 20:53:32 -0500

    load urls

--- scripts/chai-legacy-loader/sql/dependencies.sql
+++ scripts/chai-legacy-loader/sql/dependencies.sql
@@ -10,5 +10,5 @@ select s.start_id, s.end_id
 from public.sources s 
 join public.projects p 
 on s.start_id = p.id 
-and 'npm' = any(p.package_managers)
+and $1 = any(p.package_managers)
 ;
--- scripts/chai-legacy-loader/sql/packages.sql
+++ scripts/chai-legacy-loader/sql/packages.sql
@@ -1,7 +1,7 @@
 select 
-	concat('npm/', project_name) as project_derived_key,
+	concat($1, '/', project_name) as project_derived_key,
 	project_name as name, 
 	id as import_id 
 from projects 
-where 'npm' = any(package_managers)
+where $1 = any(package_managers)
 ;
