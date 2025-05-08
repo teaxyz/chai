f3f8d6f468138b164f1df612d693502e34cf9fc0
Sanchit Ram Arvind <sanchitram@gmail.com>
2025-05-08 10:01:26 -0400

    url loader (#87)
    * insert urls
    
    * insert package urls
    
    * lint
    
    * remove old

--- scripts/chai-legacy-loader/sql/urls.sql
+++ scripts/chai-legacy-loader/sql/urls.sql
@@ -3,4 +3,7 @@ select
 	"source", 
 	homepage 
 from projects
-where $1 = any(package_managers)
+where 
+	'npm' = any(package_managers)
+	and created_at < '2024-01-01'::timestamp -- before ITN
+	and is_spam is false -- use legacy spam filter
