6f21a392eef3b609dadedb36b22b17d438875351
Sanchit Ram Arvind <sanchitram@gmail.com>
2025-06-23 16:44:34 -0500

    test_debian_parser

--- tests/package_managers/debian/test_debian_parser.py
+++ tests/package_managers/debian/test_debian_parser.py
@@ -11,42 +11,8 @@ from package_managers.debian.parser import DebianParser
 
 
 @pytest.fixture
-def multiline_binutils():
-    return """
-Package: binutils
-Binary: binutils-for-host, binutils-for-build,
- binutils-ia64-linux-gnu-dbg, binutils-m68k-linux-gnu,
- binutils-mips64el-linux-gnuabin32-dbg, binutils-mipsisa64r6-linux-gnuabin32,
- binutils-mipsisa64r6el-linux-gnuabi64-dbg
-
-"""
-
-
-@pytest.mark.parser
-class TestDebianParser:
-    """Test the Debian parser functionality."""
-
-    def test_multiline_binutils(self, multiline_binutils):
-        """Test handling of multiline binaries."""
-        parser = DebianParser(multiline_binutils)
-        sources = list(parser.parse())
-        assert len(sources) == 1
-        source = sources[0]
-        assert source.package == "binutils"
-        assert source.binary == [
-            "binutils-for-host",
-            "binutils-for-build",
-            "binutils-ia64-linux-gnu-dbg",
-            "binutils-m68k-linux-gnu",
-            "binutils-mips64el-linux-gnuabin32-dbg",
-            "binutils-mipsisa64r6-linux-gnuabin32",
-            "binutils-mipsisa64r6el-linux-gnuabi64-dbg",
-        ]
-
-    def test_parse_package_data(self):
-        """Test parsing a typical package entry from Packages file."""
-        # Sample package data from a Packages file
-        package_data = """Package: 0ad
+def simple_package():
+    return """Package: 0ad
 Version: 0.0.26-1
 Installed-Size: 19162
 Maintainer: Debian Games Team <pkg-games-devel@lists.alioth.debian.org>
@@ -65,6 +31,81 @@ SHA256: 9da19833c1a51e890aa8a11f82ec1e383c0e79410c3d2f6845fd2ec3e23249b8
 
 
 """
+
+
+@pytest.fixture
+def simple_source():
+    return """Package: 0ad
+Binary: 0ad, 0ad-dbg, 0ad-data, 0ad-data-common
+Version: 0.0.26-1
+Maintainer: Debian Games Team <pkg-games-devel@lists.alioth.debian.org>
+Uploaders: Vincent Cheng <vcheng@debian.org>, Euan Kemp <euank@euank.com>
+Build-Depends: debhelper-compat (= 13), cmake, dpkg-dev (>= 1.15.5), libboost-dev, libenet-dev (>= 1.3), libopenal-dev, libpng-dev, libsdl2-dev, libtiff5-dev, libvorbis-dev, libxcursor-dev, pkg-config, zlib1g-dev, libcurl4-gnutls-dev, libgloox-dev, libjsoncpp-dev, libminiupnpc-dev, libnspr4-dev, libnss3-dev, libsodium-dev, libwxgtk3.0-gtk3-dev | libwxgtk3.0-dev, python3, python3-dev, libxml2-dev, rust-gdb [amd64 i386 ppc64el]
+Architecture: any all
+Standards-Version: 4.5.1
+Format: 3.0 (quilt)
+Files:
+ 2fc0f38b8a4cf56fea7040fcf5f79ca3 2414 0ad_0.0.26-1.dsc
+ 35ca57e781448c69ba31323313e972af 31463733 0ad_0.0.26.orig.tar.xz
+ f78de44c8a9c32e6be3ae99f2747c330 71948 0ad_0.0.26-1.debian.tar.xz
+Vcs-Browser: https://salsa.debian.org/games-team/0ad
+Vcs-Git: https://salsa.debian.org/games-team/0ad.git
+Directory: pool/main/0/0ad
+Priority: optional
+Section: games
+Testsuite: autopkgtest
+Testsuite-Triggers: g++, pyrex
+
+
+"""
+
+
+@pytest.fixture
+def multiline_binary():
+    return """
+Package: binutils
+Binary: binutils-for-host, binutils-for-build,
+ binutils-ia64-linux-gnu-dbg, binutils-m68k-linux-gnu,
+ binutils-mips64el-linux-gnuabin32-dbg, binutils-mipsisa64r6-linux-gnuabin32,
+ binutils-mipsisa64r6el-linux-gnuabi64-dbg
+
+"""
+
+
+@pytest.fixture
+def platform_specified_build_depends():
+    return """
+Package: example
+Build-Depends: gcc-11-source (>= 11.3.0-11~), gawk, gettext, gperf (>= 3.0.1), lib32gcc1-amd64-cross [amd64 arm64 i386 ppc64el x32], g++-11, gccgo-11, gm2-11 [!powerpc !ppc64 !x32]
+"""
+
+
+@pytest.mark.parser
+class TestDebianParser:
+    """Test the Debian parser functionality."""
+
+    def test_multiline_binary(self, multiline_binary):
+        """Test handling of multiline binaries."""
+        parser = DebianParser(multiline_binary)
+        sources = list(parser.parse())
+        assert len(sources) == 1
+        source = sources[0]
+        assert source.package == "binutils"
+        assert source.binary == [
+            "binutils-for-host",
+            "binutils-for-build",
+            "binutils-ia64-linux-gnu-dbg",
+            "binutils-m68k-linux-gnu",
+            "binutils-mips64el-linux-gnuabin32-dbg",
+            "binutils-mipsisa64r6-linux-gnuabin32",
+            "binutils-mipsisa64r6el-linux-gnuabi64-dbg",
+        ]
+
+    def test_parse_package_data(self, simple_package):
+        """Test parsing a typical package entry from Packages file."""
+        # Sample package data from a Packages file
+        package_data = simple_package
+
         # Parse the package data
         parser = DebianParser(package_data)
         packages = list(parser.parse())
@@ -96,33 +137,9 @@ SHA256: 9da19833c1a51e890aa8a11f82ec1e383c0e79410c3d2f6845fd2ec3e23249b8
         assert len(package.suggests) == 1
         assert package.suggests[0].package == "0ad-dbg"
 
-    def test_parse_source_data(self):
+    def test_parse_source_data(self, simple_source):
         """Test parsing a typical source entry from Sources file."""
-        # Sample source data from a Sources file
-        source_data = """Package: 0ad
-Binary: 0ad, 0ad-dbg, 0ad-data, 0ad-data-common
-Version: 0.0.26-1
-Maintainer: Debian Games Team <pkg-games-devel@lists.alioth.debian.org>
-Uploaders: Vincent Cheng <vcheng@debian.org>, Euan Kemp <euank@euank.com>
-Build-Depends: debhelper-compat (= 13), cmake, dpkg-dev (>= 1.15.5), libboost-dev, libenet-dev (>= 1.3), libopenal-dev, libpng-dev, libsdl2-dev, libtiff5-dev, libvorbis-dev, libxcursor-dev, pkg-config, zlib1g-dev, libcurl4-gnutls-dev, libgloox-dev, libjsoncpp-dev, libminiupnpc-dev, libnspr4-dev, libnss3-dev, libsodium-dev, libwxgtk3.0-gtk3-dev | libwxgtk3.0-dev, python3, python3-dev, libxml2-dev, rust-gdb [amd64 i386 ppc64el]
-Architecture: any all
-Standards-Version: 4.5.1
-Format: 3.0 (quilt)
-Files:
- 2fc0f38b8a4cf56fea7040fcf5f79ca3 2414 0ad_0.0.26-1.dsc
- 35ca57e781448c69ba31323313e972af 31463733 0ad_0.0.26.orig.tar.xz
- f78de44c8a9c32e6be3ae99f2747c330 71948 0ad_0.0.26-1.debian.tar.xz
-Vcs-Browser: https://salsa.debian.org/games-team/0ad
-Vcs-Git: https://salsa.debian.org/games-team/0ad.git
-Directory: pool/main/0/0ad
-Priority: optional
-Section: games
-Testsuite: autopkgtest
-Testsuite-Triggers: g++, pyrex
-
-
-"""
-        # Parse the source data
+        source_data = simple_source
         parser = DebianParser(source_data)
         sources = list(parser.parse())
 
@@ -162,28 +179,3 @@ Testsuite-Triggers: g++, pyrex
         assert source.vcs_git == "salsa.debian.org/games-team/0ad"
         assert source.testsuite == "autopkgtest"
         assert source.testsuite_triggers == "g++, pyrex"
-
-    def test_handle_uploaders(self):
-        """Test handling of uploaders with special characters and edge cases."""
-        maintainer = """Package: example
-Uploaders: "Adam C. Powell, IV" <hazelsct@debian.org>, Drew Parsons <dparsons@debian.org>"""
-        parser = DebianParser(maintainer)
-        sources = list(parser.parse())
-        assert len(sources) == 1
-        source = sources[0]
-        assert len(source.uploaders) == 2
-        assert source.uploaders[0].name == "Adam C. Powell, IV"
-        assert source.uploaders[0].email == "hazelsct@debian.org"
-        assert source.uploaders[1].name == "Drew Parsons"
-        assert source.uploaders[1].email == "dparsons@debian.org"
-
-        maintainer = """Package: calamares-extensions
-Binary: calamares-extensions, calamares-extensions-data
-Version: 1.2.1-2
-Maintainer: Debian KDE Extras Team <pkg-kde-extras@lists.alioth.debian.org>,"""
-        parser = DebianParser(maintainer)
-        sources = list(parser.parse())
-        assert len(sources) == 1
-        source = sources[0]
-        assert source.maintainer.name == "Debian KDE Extras Team"
-        assert source.maintainer.email == "pkg-kde-extras@lists.alioth.debian.org"
